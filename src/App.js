598581a2c7f4618714e3d67b4ac002b7c2cc7eff
Pamintuan, Russel <russel.pamintuan@accenture.com>
2024-05-22 19:25:46 +0800

    update hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
-import PokedexHome from "./components/PokedexHome";
 import PokedexMain from "./components/PokedexMain";
-import PokeItems from "./components/PokeItems";
-import PokeModal from "./components/PokeModal";
-import { BrowserRouter, Route, Routes, Navigate, Outlet } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const [poke, setPoke] = useState(false);
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <PokedexMain />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
